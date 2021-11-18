ca181017b81f5a55267c8653a623ca31d502166b
kausalya17897 <kausalya1787@mail.com>
2021-11-19 00:20:06 +0530

    bootstrap pricing table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import CheckIcon from '@mui/icons-material/Check';
+import { useState } from 'react';
+
+ export default function App() {
+  const data=[{
+    access:"FREE",price:"$0/",sub:"month",user:"Single User",
+    storage:"5GB Storage",projects:"Unlimited Public Projects",group:"Community Access",project:"Unlimited Private Projects",support:"Dedicated Phone Support",domain:"Free Subdomain",report:"Monthly Status Reports",
+  },{
+    access:"PLUS",price:"$9/",sub:"month",user:"5 Users",
+    storage:"50GB Storage",projects:"Unlimited Public Projects",group:"Community Access",project:"Unlimited Private Projects",support:"Dedicated Phone Support",domain:"Free Subdomain",report:"Monthly Status Reports",
+  },{
+    access:"PRO",price:"$49/",sub:"month",user:"Unlimited Users",
+    storage:"150GB Storage",projects:"Unlimited Public Projects",group:"Community Access",project:"Unlimited Private Projects",support:"Dedicated Phone Support",domain:"Unlimited Free Subdomain",report:"Monthly Status Reports",
+  },
+]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      {data.map((ur)=>(
+      <Msg access={ur.access}
+      price={ur.price}
+  sub={ur.sub}
+  user={ur.user}
+  group={ur.group}
+  storage={ur.storage}
+  projects={ur.projects}
+  project={ur.project}
+  support={ur.support}
+  domain={ur.domain}
+  report={ur.report}/>
+  ))}
+  
+  </div>
+  );}
 
-export default App;
+function Msg({access,price,sub,user,projects,group,project,storage,support,domain,report,}){
+  
+  return(
+    
+      <div className="container">
+        <h1 className="access">{access}</h1>
+        <h3 className="price">{price}
+ <sub className="sub">{sub}</sub></h3>
+ <hr></hr>
+ <ul>
+   <li className="check">{user}</li>
+   <li  className="check">{storage}</li>
+   <li className="check">{projects}</li>
+   <li className="check">{group}</li>
+ <li>{project}</li>
+ <li>{support}</li>
+ <li>{domain}</li>
+ <li>{report}</li>
+ </ul>
+ <button className="button">BUTTON</button>
+    </div>
+
+  )
+}
